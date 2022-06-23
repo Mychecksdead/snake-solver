c8cf5ec4d215472d2718d0d6a3b936d459583f95
Mert Koksal <85784378+Mychecksdead@users.noreply.github.com>
2022-06-23 17:17:37 +0300

    Better scores

--- sketch.js
+++ sketch.js
@@ -1,59 +1,70 @@
 
-let turn, s, bait, as, size;
+let turn, s, bait, as, size, color;
 function setup() {
     createCanvas(400, 400);
-    stroke(255);
+    stroke(0);
     fill(255);
     background(0); 
     turn = 0;
     size = 10;
     s = new Snake();
     bait = new Bait();
-    as = new AutonomSnake(bait.pos[0] + height/size/2, bait.pos[1] + height/size/2);
+    as = new AutonomSnake(bait.pos[0], bait.pos[1]);
     as.findPath(s.blocks);
 }
 
+let l = 0, r = 1, u = 2, d = 3;
 
 function draw() {
-    clear(); background(0); frameRate(10); translate(width/2, height/2);
+    clear(); background(0);
+    frameRate(20);
     
-    s.renderSnake();
+    as.findPath(s.blocks, s.full);
+
+    as.autonomousMove(s.pos[0], s.pos[1]);
+    s.move();    
+
     bait.renderBait();
+    s.renderSnake();
+
     
-    as.autonomousMove(s.pos[0] +  height/size/2, s.pos[1] +  height/size/2);
-    s.move();
 
     if(bait.pos[0] == s.pos[0] && bait.pos[1] == s.pos[1]){
-        s.renderSnake();
-        bait.renderBait();
-        bait.eaten();
+        bait.eaten(s.blocks);
         s.full = 1;
-        as.targetX = bait.pos[0] + height/size/2;
-        as.targetY = bait.pos[1] + height/size/2;
-        as.findPath(s.blocks);
+        as.targetX = bait.pos[0];
+        as.targetY = bait.pos[1];
     }
-    
 } 
 function keyPressed(){
-    if(keyCode == RIGHT_ARROW && turn != 2) turn = 0;
-    if(keyCode == LEFT_ARROW && turn != 0) turn = 2;
-    if(keyCode == DOWN_ARROW && turn != 1) turn = 3;
-    if(keyCode == UP_ARROW && turn != 3) turn = 1;
+    if(keyCode == RIGHT_ARROW && turn != l) turn = r;
+    if(keyCode == LEFT_ARROW && turn != r) turn = l;
+    if(keyCode == DOWN_ARROW && turn != u) turn = d;
+    if(keyCode == UP_ARROW && turn != d) turn = u;
 }
 
 class Bait{
     constructor(){
-        this.pos = [Math.floor(random(-width/2, width/2) / size), Math.floor(random(-height/2, height/2) / size)];
+        this.pos = [Math.floor(random(0, width) / size), Math.floor(random(0, height) / size)];
     }
     renderBait(){
         fill('red');
-        stroke('red');
+        stroke('black');
         rect(this.pos[1] * size, this.pos[0] * size, size, size);
         fill('white');
-        stroke('white');
+        stroke('black');
     }
-    eaten(){
-        this.pos = [Math.floor(random(-width/2, width/2) / size), Math.floor(random(-height/2, height/2) / size)];
+    eaten(snake){
+        let equal = 0;
+        while(!equal){
+            equal = 1;
+            this.pos = [Math.floor(random(0, width) / size), Math.floor(random(0, height) / size)];
+            for(let i = 0; i < snake.length; ++i)
+                if(snake[i][0] == this.pos[0] && snake[i][1] == this.pos[1]){
+                    equal = 0;
+                    break;
+                }
+            }
     }
 }
 
@@ -65,24 +76,26 @@ class Snake{
         this.blocks = [[0, 0]];
         this.pos = this.blocks[0];
         this.full = 0;
+        this.lost = 0;
     }
     renderSnake(){
         for(let i = 0; i < this.size; ++i){
-            this.blocks[i][0] = ((this.blocks[i][0] + height/size/2 + height/size) % (height/10)) - height/size/2;
-            this.blocks[i][1] = ((this.blocks[i][1] + height/size/2 + height/size) % (height/10)) - height/size/2;
             rect(this.blocks[i][1] * size, this.blocks[i][0] * size, size, size);
         }
     }
     move(){
         let newx = this.blocks[0][0], newy = this.blocks[0][1];
-        if(turn == 0) //right
+        if(turn == r) //right
             newy += 1;
-        else if(turn == 1) //up
+        else if(turn == u) //up
             newx -= 1;
-        else if(turn == 2) //left
+        else if(turn == l) //left
             newy -= 1;
         else   //down
-          newx += 1;
+            newx += 1;
+        newx = (newx + height/size) % (height/size);
+        newy = (newy + width/size) % (width/size);      
+ 
         this.blocks.unshift([newx, newy]);
         if(s.full != 1) this.blocks.pop();
         else s.full = 0, this.size += 1;
@@ -92,6 +105,7 @@ class Snake{
             if(newx == this.blocks[i][0] && newy == this.blocks[i][1]){
                 this.blocks = [[0, 0]];
                 this.size = 1;
+                this.lost = 1;
                 break;
             }
         }
@@ -108,42 +122,43 @@ class AutonomSnake{
         for(let i = 0; i < height/size; i++){
             this.parent.push([]);
             this.field.push([]);
-            for(let j = 0; j < height/size; j++){
+            for(let j = 0; j < width/size; j++){
                 this.parent[i].push([-1, -1]);
                 this.field[i].push(0);
             }
         }
     }
-    f(num){
-        return (num+40)%40;
-    }
-    findPath(snake_blocks){
+    findPath(snake_blocks, is_full){
         this.parent = [];
         this.field = [];
         for(let i = 0; i < height/size; i++){
             this.parent.push([]);
             this.field.push([]);
-            for(let j = 0; j < height/size; j++){
+            for(let j = 0; j < width/size; j++){
                 this.parent[i].push([-1, -1]);
                 this.field[i].push(0);
             }
         }
 
-        for(let i = 0; i < snake_blocks.length; ++i) this.field[snake_blocks[i][0] + height/size/2][snake_blocks[i][1] + height/size/2] = 1;
-
+        for(let i = 1; i < snake_blocks.length; ++i) this.field[snake_blocks[i][0]][snake_blocks[i][1]] = (snake_blocks.length - i + is_full);
+        
         let q = [[this.targetX, this.targetY]];
         let arr = [[1, 0], [0, 1], [-1, 0], [0, -1]];
 
-        this.field[this.targetX][this.targetY] = 1;
+        this.field[this.targetX][this.targetY] = height*width+2;
         this.parent[this.targetX][this.targetY] = [this.targetX, this.targetY];
         
         while(q.length > 0){
             let pos = q.shift();
             for(let i = 0; i < 4; ++i){
-                let a = this.f(pos[0] + arr[i][0]);
-                let b = this.f(pos[1] + arr[i][1]);
-                if(this.field[a][b] == 0){
-                    this.field[a][b] = 1;
+                let a = pos[0] + arr[i][0];
+                let b = pos[1] + arr[i][1];
+                a = (a + height/size) % (height/size);
+                b = (b + width/size) % (width/size);
+
+                let dist = abs(a - snake_blocks[0][0]) + abs(b - snake_blocks[0][1]);
+                if(this.field[a][b] <= dist){
+                    this.field[a][b] = height*width+2;
                     this.parent[a][b] = pos;
                     q.push([a, b]);
                 }
@@ -151,16 +166,23 @@ class AutonomSnake{
         }
     }
     autonomousMove(x, y){
-        if(this.parent[x][y][0] < x && turn != 3){
-            turn = 1;
-        }else if(this.parent[x][y][0] > x && turn != 1){
-            turn = 3;
-        }else if(this.parent[x][y][1] < y && turn != 0){
-            turn  = 2;
-        }else if(this.parent[x][y][1] > y && turn != 2){
-            turn = 0;
+        let a = this.parent[x][y][0];
+        let b = this.parent[x][y][1];
+        if(a == 0 && x == height/size - 1) a = height/size;
+        if(a == height/size - 1 && x == 0) x = height/size;
+        if(b == 0 && y == width/size - 1) b = width/size;
+        if(b == width/size - 1 && y == 0) y = width/size;
+
+        if(a < x && turn != d){
+            turn = u;
+        }else if(a > x && turn != u){
+            turn = d; 
+        }else if(b < y && turn != r){
+            turn = l;
+        }else if(b > y && turn != l){
+            turn = r;
         }else{
-            console.log("Can't move for now....");
+            console.log("IMPOSSIBLE");  
         }
     }
 
